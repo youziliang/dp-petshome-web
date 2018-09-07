d37fbb994c18efe80904f141e85c5e98df15f51f
youziliang <345717321@qq.com>
2018-09-07 17:05:08 +0800

    ...

--- src/main/java/com/dp/petshome/web/ActivityController.java
+++ src/main/java/com/dp/petshome/web/ActivityController.java
@@ -1,6 +1,8 @@
 package com.dp.petshome.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -52,8 +55,9 @@ public class ActivityController {
 	public HttpResult<Object> signUp(HttpServletRequest request, HttpServletResponse response) {
 
 		HttpResult<Object> result = new HttpResult<>();
-		String activityId = request.getParameter("activityId");
 		try {
+			String activityId = request.getParameter("activityId");
+
 			String tempUserId = CookieUtil.getCookie(request, USER_ID);
 			HashMap<String, String> importances = (HashMap<String, String>) ehCacheUtil.get(USER_CACHE, tempUserId);
 			String openid = importances.get("openid");
@@ -69,12 +73,57 @@ public class ActivityController {
 				}
 			}
 			int signUpResult = activityService.signUp(Integer.valueOf(activityId), userId);
-			log.info("活動: " + activityId + "报名活动结果: {}", signUpResult);
+			log.info("活動: {} 的报名活动结果: {}", activityId, signUpResult);
 			if (signUpResult > 0) {
 				result.setStatus(HttpStatus.SUCCESS.status);
 			} else {
 				result.setStatus(HttpStatus.FAIL.status);
 			}
+		} catch (Exception e) {
+			log.error("报名活动异常: {}", e);
+			result.setStatus(HttpStatus.EXCEPTION.status);
+		}
+		return result;
+	}
+
+	/**
+	 * @Description 加載已報名用戶
+	 */
+	@SuppressWarnings("unchecked")
+	@GetMapping(value = "loadSignUpUsers")
+	@ResponseBody
+	public HttpResult<Object> loadSignUpUsers(HttpServletRequest request, HttpServletResponse response) {
+
+		HttpResult<Object> result = new HttpResult<>();
+		List<User> list = new ArrayList<>();
+		try {
+			String tempUserId = CookieUtil.getCookie(request, USER_ID);
+			HashMap<String, String> importances = (HashMap<String, String>) ehCacheUtil.get(USER_CACHE, tempUserId);
+
+			String activityId = request.getParameter("activityId");
+			String signUp = activityService.getSignUpUserById(Integer.valueOf(activityId));
+			String[] userIds = StringUtils.split(signUp, ",");
+
+			if (null != importances && 0 == userService.getUserByOpenid(importances.get("openid")).getRole()) {
+				// 登陸狀態并且是管理員管理員可以看到全手機號
+				for (String userId : userIds) {
+					User signUpUser = userService.getUserInfo(Integer.valueOf(userId));
+					list.add(signUpUser);
+				}
+			} else {
+				// 未登錄狀態或非管理員只能看到部分手機號
+				for (String userId : userIds) {
+					User signUpUser = userService.getUserInfo(Integer.valueOf(userId));
+					String tel = signUpUser.getTel();
+					StringBuffer sb = new StringBuffer();
+					String telHidden = sb.append(StringUtils.substring(tel, 0, 3)).append("****").append(StringUtils.substring(tel, 7, 11)).toString();
+					signUpUser.setTel(telHidden);
+					list.add(signUpUser);
+				}
+			}
+			log.info("活動: {} 的报名用户列表: {}", activityId, list);
+			result.setStatus(HttpStatus.SUCCESS.status);
+			result.setData(list);
 		} catch (Exception e) {
 			log.error("报名活动异常: {}", e);
 			result.setStatus(HttpStatus.EXCEPTION.status);
